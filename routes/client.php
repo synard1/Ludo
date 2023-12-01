f529b088a7902d41715755e9e699e677b2eeacf1
Mhd. Iqbal Syahputra <synard1@gmail.com>
2023-12-01 13:13:29 +0700

    fix client domain

--- routes/client.php
+++ routes/client.php
@@ -5,6 +5,6 @@ use App\Http\Controllers\SettingController;
 
 Route::group(['domain' => '{client_id}.ludo.my.id', 'prefix' => 'c'], function () {
     // Your routes go here
-    Route::get('/test', 'SettingController@domainCheck');
+    Route::get('/test', [SettingController::class, 'domainCheck'])->name('domainCheck');
 });
 
