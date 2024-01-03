c13d84a281f416425cb1def1a0a09ba91877ee01
Mhd. Iqbal Syahputra <synard1@gmail.com>
2024-01-03 11:11:01 +0700

    init sla

---
+++ Modules/SLA/Database/Seeders/SLADatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\SLA\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class SLADatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
